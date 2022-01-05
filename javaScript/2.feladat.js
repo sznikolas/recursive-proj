b443be5fff558a78f767a06029c98803e111911e
Nikolas Szocs <szocs.nikolas@gmail.com>
2022-01-05 23:37:31 +0100

    frissites

--- javaScript/2.feladat.js
+++ javaScript/2.feladat.js
@@ -1,36 +1,48 @@
 //első canvas
 let canvas = document.getElementById('canvas2');
 let c = canvas.getContext('2d');
-canvas.width = 360;
-canvas.height  = 360;
+canvas.width = 540;
+canvas.height  = 450;
 
 let counter = 1;
 
-let parapa = ["", "2. lépés: ", "3. lépés: ", "4. lépés: ","5. lépés: ","6. lépés: ","7. lépés: ","8. lépés: ",
-"9. lépés: ","10. lépés: ","11. lépés: ","12. lépés: ","13. lépés: ","14. lépés: ","15. lépés: ","16. lépés: ","17. lépés: ",
-"18. lépés: ","19. lépés: ","20. lépés: ","21. lépés: ","22. lépés: ",
+let parapa = ["", "Helyezzük a második királynőt a 2. sorba úgy, hogy ne tudja megtámadni őt az első sorban lévő királynő.", 
+"(1,0) nem lehetséges","(1,1) nem lehetséges","(1,2) lehetséges",
 
-"23. lépés: ","24. lépés: ","25. lépés: ",
-"26. lépés: ",
-"27. lépés: ", "29. lépés: ", "30. lépés: ","31. lépés: ",
-"32. lépés: ","33. lépés: ","34. lépés: ","35. lépés: Az N királynőt sikeresen lehelyeztük!"];
+"Helyezzük a harmadik királynőt a 3. sorba úgy, hogy ne tudja megtámadni őt az első két sorban lévő királynő.",
+"(2,0) nem lehetséges","(2,1) nem lehetséges","(2,2) nem lehetséges","(2,3) nem lehetséges",
+"A 3. sorba elhelyezés nem lehetséges, ezért BACKTRACK a 2. sorba.", 
 
+"Helyezze a második királynőt a 2. sor egy másik biztonságos cellájába.",
+"(1,3) lehetséges",
 
+"Helyezzük a harmadik királynőt a 3. sorba úgy, hogy ne tudja megtámadni őt az első két sorban lévő királynő.",
+"(2,0) nem lehetséges","(2,1) lehetséges",
 
-/* //második canvas
-let canvas2 = document.getElementById('canvas1_2');
-let c2 = canvas2.getContext('2d');
-canvas2.width = 360;
-canvas2.height  = 360;
+"Helyezze a negyedik királynőt a 4. sorba úgy, hogy ne tudja megtámadni őt az első három sorban lévő királynő.",
+"(3,0) nem lehetséges","(3,1) nem lehetséges","(3,2) nem lehetséges","(3,3) nem lehetséges",
+"A 4. sorba elhelyezés nem lehetséges, ezért BACKTRACK a 3. sorba.",
 
-let counter2 = 0;
-let alrdyCounter=0;
-let corX = [0,90,180,360,270,   90,180,0,90,270,    360,270,90,0,180,   360,270,90,0,180,   360,270,360,180,0];
-let corY = [0,180,360,270,90,   0,180,270,90,0,     180,360,270,90,0,   90,270,360,180,90,  0,180,360,270,360];
+"Helyezze a harmadik királynőt egy másik biztonságos cellába a 3. sorban.",
+"(2,2) nem lehetséges","(2,3) nem lehetséges",
 
-let alrdyX = [0+5,90+5,180+5,360+5,270+5,   90+5,180+5,0+5,90+5,270+5,    360+5,270+5,90+5,0+5,180+5,   360+5,270+5,90+5,0+5,180+5,   360+5,270+5,360+5,180+5,]; 
-let alrdY = [0+5,180+5,360+5,270+5,90+5,   0+5,180+5,270+5,90+5,0+5,     180+5,360+5,270+5,90+5,0+5,   90+5,270+5,360+5,180+5,90+5,  0+5,180+5,360+5,270+5,]; */
+"A 3. sorba elhelyezés nem lehetséges, ezért BACKTRACK a 2. sorba.", 
+"Helyezze a második királynőt a 2. sor egy másik biztonságos cellájába.",
+"A 2. sorba elhelyezés nem lehetséges, ezért BACKTRACK a 1. sorba.", 
 
+"Helyezze az első királynőt az 1. sor egy másik biztonságos cellájába.",
+"(0,1) lehetséges",
+
+"Helyezzük a második királynőt a 2. sorba úgy, hogy ne tudja megtámadni őt az első sorban lévő királynő.",
+"(1,0) nem lehetséges","(1,1) nem lehetséges","(1,2) nem lehetséges","(1,3) lehetséges",
+
+"Helyezzük a harmadik királynőt a 3. sorba úgy, hogy ne tudja megtámadni őt az első két sorban lévő királynő.",
+"(2,0) lehetséges",
+
+"Helyezze a negyedik királynőt a 4. sorba úgy, hogy ne tudja megtámadni őt az első három sorban lévő királynő.",
+"(3,0) nem lehetséges","(3,1) nem lehetséges","(3,2) lehetséges",
+
+"A 4 királynő lehelyezése sikeresen megtörtént!",];
 
 let buttonState = "START";
 
@@ -38,6 +50,9 @@ let isAllowed = false;
 
 const knight = new Image();
 knight.src = "images/Queen2.png";
+
+const noQueen = new Image();
+noQueen.src = "images/noQueen.png";
 
 //canvas1
 let myAnim;
@@ -50,175 +65,174 @@ function animate(){
 
     //0
     if (counter === 1) {
-        c.drawImage(knight,0,0);
+        c.drawImage(knight,90,90);
 
     };
     //1
-    if (counter === 2) {
-        c.drawImage(knight,180,90);
-       // c.fillRect(5,5,80,80);
-
-    };
-
     if (counter === 3) {
-        c.drawImage(knight,90,270);      
-
+    c.drawImage(noQueen,90,180);
+   
     };
 
     if (counter === 4) {
-             
-        c.clearRect(90,270,90,90)
+        c.drawImage(noQueen,180,180);     
+
     };
 
     if (counter === 5) {
-        c.clearRect(180,90,90,90);   
-    };
+        c.drawImage(knight,270,180);
 
-    if (counter === 6) {
-             
-        c.drawImage(knight,270,90);  
     };
 
     if (counter === 7) {
-             
-        c.drawImage(knight,90,180);  
+        
+        c.drawImage(noQueen,90,270);
     };
 
     if (counter === 8) {
-             
-        c.clearRect(90,180,90,90); 
+        c.drawImage(noQueen,180,270);
+
     };
 
     if (counter === 9) {
-             
-        c.drawImage(knight,180,270);  
+        c.drawImage(noQueen,270,270);
     };
 
     if (counter === 10) {
-             
-        c.clearRect(180,270,90,90);; 
+        c.drawImage(noQueen,360,270);
+
     };
 
     if (counter === 11) {
-             
-        c.clearRect(270,90,90,90);; 
+        c.clearRect(90,270,360,90);
+
     };
 
     if (counter === 12) {
-             
-        c.drawImage(knight,90,180);  
+        c.clearRect(270,180,90,90);
+       
+        c.drawImage(noQueen,270,180);
     };
 
     if (counter === 13) {
-             
-        c.drawImage(knight,270,90);  
+        
+        c.drawImage(knight,360,180);
+        
     };
 
     if (counter === 14) {
-             
-        c.clearRect(270,90,90,90);; 
+        c.clearRect(270,180,90,90);
+        c.drawImage(knight,360,180);
+        c.drawImage(noQueen,270,180);
     };
 
     if (counter === 15) {
-             
-        c.clearRect(90,180,90,90);; 
+        
+        c.drawImage(noQueen,90,270);
     };
-
+    
     if (counter === 16) {
-             
-        c.drawImage(knight,270,180);  
+        
+        c.drawImage(knight,180,270);
+
     };
 
-    if (counter === 17) {
-             
-        c.drawImage(knight,90,270);  
-    };
-
-    if (counter === 18) {
-             
-        c.clearRect(90,270,90,90); 
+    if (counter === 18) {      
+        c.drawImage(noQueen,90,360);
     };
 
     if (counter === 19) {
-             
-        c.clearRect(270,180,90,90);;
+        c.drawImage(noQueen,180,360);
     };
 
     if (counter === 20) {
-             
-        c.drawImage(knight,90,270);  
+        c.drawImage(noQueen,270,360);
     };
 
     if (counter === 21) {
-             
-        c.drawImage(knight,180,90);  
+        c.drawImage(noQueen,360,360);
     };
 
     if (counter === 22) {
-             
-        c.clearRect(180,90,90,90); 
+        
+        c.clearRect(90,360,360,90);
+    
     };
 
-    if (counter === 23) {
-             
-        c.drawImage(knight,270,180);  
+    if (counter === 23) {      
+        c.clearRect(180,270,90,90);
+        c.drawImage(noQueen,180,270);
     };
 
     if (counter === 24) {
-             
-        c.clearRect(270,180,90,90);
+        c.drawImage(noQueen,270,270);
     };
 
     if (counter === 25) {
-             
-        c.clearRect(90,270,90,90);
+        c.drawImage(noQueen,360,270);
     };
 
     if (counter === 26) {
-             
-        c.drawImage(knight,180,270);  
+        c.clearRect(90,270,360,90);
+
     };
 
     if (counter === 27) {
-             
-        c.drawImage(knight,270,90);  
+        c.clearRect(360,180,90,90);
+        c.drawImage(noQueen,360,180);
+
     };
+
+
 
     if (counter === 28) {
-             
-        c.clearRect(270,90,90,90);; 
+        c.clearRect(90,180,360,90);
+
     };
 
-    if (counter === 29) {
-             
-        c.clearRect(180,270,90,90); 
+    if (counter === 29) {    
+        c.clearRect(90,90,90,90);
+        c.drawImage(noQueen,90,90);
     };
 
-    if (counter === 30) {
-             
-        c.clearRect(0,0,90,90);
-    };
-
-    if (counter === 31) {
-             
-        c.drawImage(knight,90,0);  
+    if (counter === 30) {    
+        c.drawImage(knight,180,90);
     };
 
     if (counter === 32) {
-             
-        c.drawImage(knight,270,90);  
+        c.drawImage(noQueen,90,180);
     };
 
     if (counter === 33) {
-             
-        c.drawImage(knight,0,180);  
+        c.drawImage(noQueen,180,180);
     };
 
     if (counter === 34) {
-             
-        c.drawImage(knight,180,270);  
+        c.drawImage(noQueen,270,180);
     };
-    
+
+    if (counter === 35) {
+        c.drawImage(knight,360,180);
+    };
+
+    if (counter === 37) {
+        c.drawImage(knight,90,270);
+    };
+
+    if (counter === 39) {
+        c.drawImage(noQueen,90,360);
+    };
+
+    if (counter === 40) {
+        c.drawImage(noQueen,180,360);     
+    };
+
+    if (counter === 41) {   
+        c.drawImage(knight,270,360);
+    };
+
+  
+   
 
     //c.drawImage(knight,corX[counter],corY[counter]);
     myAnim = requestAnimationFrame(animate); 
@@ -227,77 +241,36 @@ myAnim = requestAnimationFrame(animate);
 animate();
 
 
-/* //canvas2
-function animate2(){
-    c2.beginPath();
-    c2.fillStyle = "rgb(62, 96, 150)";
-    c2.fillRect(alrdyX[alrdyCounter-1],alrdY[alrdyCounter-1],80, 80);
-    c2.stroke();
-    c2.closePath();
-    c2.drawImage(knight,corX[counter2],corY[counter2]);
-    myAnim2 = requestAnimationFrame(animate2); 
-}
-myAnim2 = requestAnimationFrame(animate2);
-animate2(); */
-
-
-
 //Előrelépések 1-es canvason
 document.getElementById('stepFrwd').onclick = function(){
     let szoveg = document.getElementById('label1');
     szoveg.innerText = parapa[counter];   
     counter++; 
-     if(counter >=34){
-        szoveg.innerText = parapa[counter]="35. lépés: Az N királynőt sikeresen lehelyeztük!";
+     if(counter >=41){
+        szoveg.innerText = parapa[counter]="A 4 királynő lehelyezése sikeresen megtörtént!";
     } 
 }
-
-/* //Előrelépések 2-es canvason
-document.getElementById('stepFrwd2').onclick = function(){
-    counter2++; 
-    alrdyCounter++;
-    myAnim2 = requestAnimationFrame(animate2);
-}
- */
-/* document.getElementById('stepPrev').onclick = function(){
-    counter--;
-} */
 
 
 //Canvas 1 reset
 document.getElementById('resetAnim').onclick = function(){
     isAllowed = false;
     c.clearRect(0,0,canvas.width, canvas.height);
-    c.drawImage(knight,0,0);
+    c.drawImage(knight,90,90);
     counter = 1;
     //label resetelese
-    document.getElementById("label1").innerHTML = "1. lépés: előre";
+    document.getElementById("label1").innerHTML = "Helyezzük a királynőt a (0,0) négyzetbe.";
     let idk = document.getElementById("startAnim");
     idk.value = "Start"
     buttonState="START";  
 }
-
-/* //Canvas 2 reset
-document.getElementById('resetAnim2').onclick = function(){
-    isAllowed2 = false;
-    c2.clearRect(0,0,canvas2.width, canvas2.height);
-    c2.drawImage(knight,corX[0],corY[0]);
-    counter2 = 0;
-    alrdyCounter=0;
-    let idk2 = document.getElementById("startAnim2");
-    idk2.value = "Start"
-    buttonState2="START";  
-}
- */
 
 
 //canvas 1 lejatszas
 const startAnim = async () => {
     // A lépések automatikus lejátszása várakozással (startAnim)
     let slider = document.getElementById('myRange');
-        for(var x = 0; x<=57; x++){
- 
-            
+        for(var x = 0; x<=42; x++){        
             if(isAllowed){
                 let szoveg = document.getElementById('label1');
                 szoveg.innerText = parapa[counter];  
@@ -305,40 +278,14 @@ const startAnim = async () => {
                 counter++;
                 await sleep(1000/slider.value);       
             }
-            if(counter >=34){
-                szoveg.innerText = parapa[counter]="35. lépés: Az N királynőt sikeresen lehelyeztük!";
+            if(counter >=42){
+                szoveg.innerText = parapa[counter]="A 4 királynő lehelyezése sikeresen megtörtént!";
             }  
         }
     }
 // A várakozáshoz szükséges constans
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve,delay))   
 
-
-
-/* //canvas 2 lejatszas
-const startAnim2 = async () => {
-    // A lépések automatikus lejátszása várakozással (startAnim)
-    let slider = document.getElementById('myRange2');
-        for(var x = 0; x<corX.length; x++){
-            if(isAllowed2){
-                c2.drawImage(knight,corX[counter2],corY[counter2]);
-                counter2++;
-                //uj a sarga kockakhoz
-                alrdyCounter++;
-                await sleep2(1000/slider.value);       
-            }  
-            if(counter2 >=24){
-                counter2=24;
-            } 
-        }
-    }
-const sleep2 = (delay2) => new Promise((resolve2) => setTimeout(resolve2,delay2)) */
-
-
-
-/* document.getElementById('stopBtn').onclick = function(){
-    isAllowed = false;
-} */
 
 //Canvas1-hez
 document.getElementById('startAnim').onclick = function(){
@@ -353,20 +300,6 @@ change = function(){
     else {elem.value = "Stop";buttonState="STOP"}
 }
 
-/* //Canvas2-hez
-document.getElementById('startAnim2').onclick = function(){
-    if(buttonState2 === "START"){isAllowed2=true}
-    if(buttonState2 === "STOP"){isAllowed2=false}
-    startAnim2();
-    change2()
-}
-    
-
-change2 = function(){
-    let elem2 = document.getElementById("startAnim2");
-    if (elem2.value=="Stop") {elem2.value = "Start";buttonState2="START"}
-    else {elem2.value = "Stop";buttonState2="STOP"}
-} */
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
