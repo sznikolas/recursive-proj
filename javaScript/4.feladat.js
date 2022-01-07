30695735d6c7b130dd059c259274aeb2b6129a0f
Nikolas Szocs <szocs.nikolas@gmail.com>
2022-01-08 00:34:00 +0100

    frissítes

--- javaScript/4.feladat.js
+++ javaScript/4.feladat.js
@@ -6,30 +6,90 @@ canvas.height  = 300;
 
 let counter = 1;
 
-let parapa = ["", "2. lépés: ", "3. lépés: ", "4. lépés: ","5. lépés: ","6. lépés: ","7. lépés: ","8. lépés: ",
-"9. lépés: ","10. lépés: ","11. lépés: ","12. lépés: ","13. lépés: ","14. lépés: ","15. lépés: ","16. lépés: ","17. lépés: ",
-"18. lépés: ","19. lépés: ","20. lépés: ","21. lépés: ","22. lépés: ",
+let parapa = ["", 
+"5. lemez mozgatása A-ról C-re nem lehetséges",
+"4. lemez mozgatása A-ról B-re nem lehetséges",
+"3. lemez mozgatása A-ról C-re nem lehetséges",
+"2. lemez mozgatása A-ról B-re nem lehetséges",
+"1. lemez mozgatása A-ról C-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása A-ról B-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása C-ről B-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"3. lemez mozgatása A-ról C-re lehetséges, folytassuk a kisebb lemezzel",
+"2. lemez mozgatása B-ről C-re nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása B-ről A-ra lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása B-ről C-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása A-ról C-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 4-es lemezzel, ahol abbahagytuk.",
+"4. lemez mozgatása A-ról B-re lehetséges, folytassuk a kisebb lemezzel",
+"3. lemez mozgatása C-ről B-re nem lehetséges, folytassuk a kisebb lemezzel",
+"2. lemez mozgatása C-ről A-ra nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása C-ről B-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása C-ről A-ra lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása B-ről A-ra lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"3. lemez mozgatása C-ről B-re lehetséges, folytassuk a kisebb lemezzel",
+"2. lemez mozgatása A-ról B-re nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása A-ról C-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása A-ról B-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása C-ről B-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 4-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 5-ös lemezzel, ahol abbahagytuk.",
+"5. lemez mozgatása A-ról C-re lehetséges, folytassuk a kisebb lemezzel",
+"4. lemez mozgatása B-ről C-re nem lehetséges, folytassuk a kisebb lemezzel",
+"3. lemez mozgatása B-ről A-ra nem lehetséges, folytassuk a kisebb lemezzel",
+"2. lemez mozgatása B-ről C-re nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása B-ről A-ra lehetséges",
 
-"23. lépés: ","24. lépés: ","25. lépés: ",
-"26. lépés: ",
-"27. lépés: ", "29. lépés: ", "30. lépés: ","31. lépés: ",
-"32. lépés: ","33. lépés: A diszkek áthelyezése sikeresen megtörtént!" ];
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása B-ről C-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása A-ról C-re lehetséges",
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"3. lemez mozgatása B-ről A-ra lehetséges, folytassuk a kisebb lemezzel",
 
+"2. lemez mozgatása C-ről A-ra nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása C-ről B-re lehetséges",
 
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása C-ről A-ra lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása B-ről A-ra lehetséges",
 
-/* //második canvas
-let canvas2 = document.getElementById('canvas1_2');
-let c2 = canvas2.getContext('2d');
-canvas2.width = 360;
-canvas2.height  = 360;
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 4-es lemezzel, ahol abbahagytuk.",
+"4. lemez mozgatása B-ről C-re lehetséges, folytassuk a kisebb lemezzel",
 
-let counter2 = 0;
-let alrdyCounter=0;
-let corX = [0,90,180,360,270,   90,180,0,90,270,    360,270,90,0,180,   360,270,90,0,180,   360,270,360,180,0];
-let corY = [0,180,360,270,90,   0,180,270,90,0,     180,360,270,90,0,   90,270,360,180,90,  0,180,360,270,360];
+"3. lemez mozgatása A-ról C-re nem lehetséges, folytassuk a kisebb lemezzel",
+"2. lemez mozgatása A-ról B-re nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása A-ról C-re lehetséges",
 
-let alrdyX = [0+5,90+5,180+5,360+5,270+5,   90+5,180+5,0+5,90+5,270+5,    360+5,270+5,90+5,0+5,180+5,   360+5,270+5,90+5,0+5,180+5,   360+5,270+5,360+5,180+5,]; 
-let alrdY = [0+5,180+5,360+5,270+5,90+5,   0+5,180+5,270+5,90+5,0+5,     180+5,360+5,270+5,90+5,0+5,   90+5,270+5,360+5,180+5,90+5,  0+5,180+5,360+5,270+5,]; */
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása A-ról B-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása C-ről B-re lehetséges",
+
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"Visszatérés a rekurzív hívásból, folytassuk a 3-as lemezzel, ahol abbahagytuk.",
+"3. lemez mozgatása A-ról C-re lehetséges, folytassuk a kisebb lemezzel",
+
+"2. lemez mozgatása B-ről C-re nem lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása B-ről A-ra lehetséges",
+
+"Visszatérés a rekurzív hívásból, folytassuk a 2-es lemezzel, ahol abbahagytuk.",
+"2. lemez mozgatása B-ről C-re lehetséges, folytassuk a kisebb lemezzel",
+"1. lemez mozgatása A-ról C-re lehetséges",
+
+"A lemezek áthelyezése sikeresen megtörtént!"];
 
 
 let buttonState = "START";
@@ -71,7 +131,7 @@ function animate(){
 
     };
     //1
-    if (counter === 2) {
+    if (counter === 6) {
 /*      c.drawImage(disc1,228,160); B oszlop 5. szint
         c.drawImage(disc2,228,180); B oszlop 4. szint
         c.drawImage(disc3,228,200); B oszlop 3. szint
@@ -82,7 +142,7 @@ function animate(){
         c.drawImage(disc1,408,240);
     };
 
-    if (counter === 3) {
+    if (counter === 8) {
 /*      c.drawImage(disc1,408,160); C oszlop 5. szint
         c.drawImage(disc2,408,180); C oszlop 4. szint
         c.drawImage(disc3,408,200); C oszlop 3. szint
@@ -93,182 +153,182 @@ function animate(){
         c.drawImage(disc2,228,240);   
     };
 
-    if (counter === 4) {
+    if (counter === 9) {
         c.clearRect(408,240,100,20);
         c.drawImage(disc1,228,220);     
 
     };
 
-    if (counter === 5) {
+    if (counter === 12) {
         c.clearRect(48,200,100,20);
         c.drawImage(disc3,408,240);     
 
     };
 
-    if (counter === 6) {
+    if (counter === 14) {
         c.clearRect(228,220,100,20);
         c.drawImage(disc1,48,200);     
 
     };
 
-    if (counter === 7) {
+    if (counter === 16) {
         c.clearRect(228,240,100,20);
         c.drawImage(disc2,408,220);     
 
     };
 
-    if (counter === 8) {
+    if (counter === 17) {
         c.clearRect(48,200,100,20);
         c.drawImage(disc1,408,200);     
 
     };
 
-    if (counter === 9) {
+    if (counter === 21) {
         c.clearRect(48,220,100,20);
         c.drawImage(disc4,228,240);     
 
     };
 
-    if (counter === 10) {
+    if (counter === 24) {
         c.clearRect(408,200,100,20);
         c.drawImage(disc1,228,220);     
 
     };
 
-    if (counter === 11) {
+    if (counter === 26) {
         c.clearRect(408,220,100,20);
         c.drawImage(disc2,48,220);     
 
     };
 
-    if (counter === 12) {
+    if (counter === 27) {
         c.clearRect(228,220,100,20);
         c.drawImage(disc1,48,200);     
 
     };
 
-    if (counter === 13) {
+    if (counter === 30) {
         c.clearRect(408,240,100,20);
         c.drawImage(disc3,228,220);     
 
     };
 
-    if (counter === 14) {
+    if (counter === 32) {
         c.clearRect(48,200,100,20);
         c.drawImage(disc1,408,240);   
 
     };
 
-    if (counter === 15) {
+    if (counter === 34) {
         c.clearRect(48,220,100,20);
         c.drawImage(disc2,228,200);   
 
     };
 
-    if (counter === 16) {
+    if (counter === 35) {
         c.clearRect(408,240,100,20);
         c.drawImage(disc1,228,180);   
 
     };
 
-    if (counter === 17) {
+    if (counter === 40) {
         c.clearRect(48,240,100,20);
         c.drawImage(disc5,408,240);   
 
     };
 
-    if (counter === 18) {
+    if (counter === 44) {
         c.clearRect(228,180,100,20);
         c.drawImage(disc1,48,240);   
 
     };
 
-    if (counter === 19) {
+    if (counter === 46) {
         c.clearRect(228,200,100,20);
         c.drawImage(disc2,408,220);   
 
     };
 
-    if (counter === 19) {
+/*     if (counter === 47) {
         c.clearRect(228,200,100,20);
         c.drawImage(disc2,408,220);   
 
-    };
+    }; */
 
     
-    if (counter === 20) {
+    if (counter === 47) {
         c.clearRect(48,240,100,20);
         c.drawImage(disc1,408,200);   
 
     };
 
-    if (counter === 21) {
+    if (counter === 50) {
         c.clearRect(228,220,100,20);
         c.drawImage(disc3,48,240);   
 
     };
 
-    if (counter === 22) {
+    if (counter === 52) {
         c.clearRect(408,200,100,20);
         c.drawImage(disc1,228,220);   
 
     };
 
-    if (counter === 23) {
+    if (counter === 54) {
         c.clearRect(408,220,100,20);
         c.drawImage(disc2,48,220);   
 
     };
 
-    if (counter === 24) {
+    if (counter === 55) {
         c.clearRect(228,220,100,20);
         c.drawImage(disc1,48,200);   
 
     };
 
-    if (counter === 25) {
+    if (counter === 59) {
         c.clearRect(228,240,100,20);
         c.drawImage(disc4,408,220);   
 
     };
 
-    if (counter === 26) {
+    if (counter === 62) {
         c.clearRect(48,200,100,20);
         c.drawImage(disc1,408,200);   
 
     };
     
-    if (counter === 27) {
+    if (counter === 64) {
         c.clearRect(48,220,100,20);
         c.drawImage(disc2,228,240);   
 
     };
 
-    if (counter === 28) {
+    if (counter === 65) {
         c.clearRect(408,200,100,20);
         c.drawImage(disc1,228,220);   
 
     };
 
-    if (counter === 29) {
+    if (counter === 68) {
         c.clearRect(48,240,100,20);
         c.drawImage(disc3,408,200);   
 
     };
 
-    if (counter === 30) {
+    if (counter === 70) {
         c.clearRect(228,220,100,20);
         c.drawImage(disc1,48,240);   
 
     };
 
-    if (counter === 31) {
+    if (counter === 72) {
         c.clearRect(228,240,100,20);
         c.drawImage(disc2,408,180);   
 
     };
 
-    if (counter === 32) {
+    if (counter === 73) {
         c.clearRect(48,240,100,20);
         c.drawImage(disc1,408,160);   
 
@@ -288,8 +348,8 @@ document.getElementById('stepFrwd').onclick = function(){
     let szoveg = document.getElementById('label1');
     szoveg.innerText = parapa[counter];   
     counter++; 
-     if(counter >=32){
-        szoveg.innerText = parapa[counter]="33. lépés: A diszkek áthelyezése sikeresen megtörtént!";
+     if(counter >=74){
+        szoveg.innerText = parapa[counter]="A lemezek áthelyezése sikeresen megtörtént!";
     }  
 }
 
@@ -305,7 +365,7 @@ document.getElementById('resetAnim').onclick = function(){
     c.drawImage(disc5,48,240);
     counter = 1;
     //label resetelese
-    document.getElementById("label1").innerHTML = "1. lépés: előre";
+    document.getElementById("label1").innerHTML = "";
     let idk = document.getElementById("startAnim");
     idk.value = "Start"
     buttonState="START";  
@@ -317,7 +377,7 @@ document.getElementById('resetAnim').onclick = function(){
 const startAnim = async () => {
     // A lépések automatikus lejátszása várakozással (startAnim)
     let slider = document.getElementById('myRange');
-        for(var x = 0; x<=32; x++){
+        for(var x = 0; x<=73; x++){
  
             
             if(isAllowed){
@@ -327,29 +387,14 @@ const startAnim = async () => {
                 counter++;
                 await sleep(1000/slider.value);       
             }
-             if(counter >=32){
-                szoveg.innerText = parapa[counter]="33. lépés: A diszkek áthelyezése sikeresen megtörtént!";
+             if(counter >=74){
+                szoveg.innerText = parapa[counter]="A lemezek áthelyezése sikeresen megtörtént!";
             } 
         }
     }
 // A várakozáshoz szükséges constans
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve,delay))   
 
-
-
-/*  visszalepesek..
-    document.getElementById('stepPrev').onclick = function(){
-    c.beginPath();  
-    c.clearRect(corX[counter],corY[counter],90, 90);
-    counter--;
-    //uj a sarga kockakhoz
-    alrdyCounter--;
-} */
-
-
-/* document.getElementById('stopBtn').onclick = function(){
-    isAllowed = false;
-} */
 
 //Canvas1-hez
 document.getElementById('startAnim').onclick = function(){
